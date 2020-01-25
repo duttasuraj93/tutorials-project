a48820b45805b60dbd2b4a88576c037ca614d049
Suraj Dutta <surajdutta@Surajs-MacBook-Pro.local>
2020-01-25 10:41:33 +0800

    adding the new react project to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './routes/Home/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    
+    render() { 
+        return ( 
+            <Router>
+                <React.Fragment>
+                    <Route exact path="/"><Home /></Route>
+                </React.Fragment>
+            </Router>
+        );
+    }
 }
-
+ 
 export default App;
---
+++ src/routes/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return ( 
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, quis!</div>
+    );
+}
+ 
+export default Home;
