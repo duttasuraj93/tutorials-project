207f67934cbace0fa3ae06ad4bc6c2d6d6095fdc
Suraj Dutta <surajdutta@Surajs-MacBook-Pro.local>
2020-01-25 10:47:40 +0800

    second commit chek

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return ( 
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, quis!</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, quis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum molestias accusamus explicabo numquam possimus. Perspiciatis tenetur labore dicta nam explicabo?</div>
     );
 }
  
